3abb23d7d147d40d6783280bf061ca81da9340f3
vrbora <venkbora@in.ibm.com>
2016-07-27 18:15:07 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Haaii, my name is ${::fqdn}": }
+  notify { "Hi, my name is ${::fqdn}": }
 }
