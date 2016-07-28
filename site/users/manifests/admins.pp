8612bcbc3195d4139095ecf570a2da19d7fb783c
vrbora <venkbora@in.ibm.com>
2016-07-28 21:55:35 +0530

    Aaron absent

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -3,9 +3,9 @@ users::managed_user { 'joe': }
 users::managed_user { 'alice':
 group => 'staff',
 }
-users::managed_user { 'aaron':
-group => 'staff',
-}
+#users::managed_user { 'aaron':
+#group => 'staff',
+#}
 group { 'staff':
 ensure => present,
 }
