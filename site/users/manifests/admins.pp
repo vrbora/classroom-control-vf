1025b5169124479e32ad45521fbd34f1a04737e3
vrbora <venkbora@in.ibm.com>
2016-07-28 22:36:19 +0530

    Aaron moved

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,11 +1,12 @@
 class users::admins {
 users::managed_user { 'joe': }
-users::managed_user { 'alice':
-group => 'staff',
-}
+users::managed_user { 'alice':}
 users::managed_user { 'aaron':
 group => 'staff',
 }
+#users::managed_user { 'aaron':
+#group => 'staff',
+#}
 group { 'staff':
 ensure => present,
 }
